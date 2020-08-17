9d097e057b2554e07e083aa1705249797eec3fa6
Sebastián Iñones <sebainones@gmail.com>
2020-08-17 18:35:33 +0200

    M220N: Basic excercis for Mongo DB coruse.

--- NetCoreMongoMflix/Program.cs
+++ NetCoreMongoMflix/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace NetCoreMongoMflix
 {
@@ -6,7 +8,23 @@ namespace NetCoreMongoMflix
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            var connString = "mongodb+srv://m220student:m220password@mflix.abmld.azure.mongodb.net/test?retryWrites=true&w=majority";
+
+            var client = new MongoClient(connString);
+
+            var db = client.GetDatabase("sample_mflix");
+            var collection = db.GetCollection<BsonDocument>("movies");
+
+            var result = collection.Find(new BsonDocument())
+               .SortByDescending(m => m["runtime"])
+               .Limit(10)
+               .ToList();
+
+            foreach(var r in result)
+            {
+                Console.WriteLine(r.GetValue("title"));
+            }
         }
     }
 }
